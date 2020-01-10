88d4381852934785d6c17cc36120370ac425b901
Juan Contreras <js.contreras@hotmail.com>
2020-01-09 21:37:39 -0500

    implememented recursive solution to day1:part2

--- day1/day1.jl
+++ day1/day1.jl
@@ -12,7 +12,7 @@ output = sum(get_fuel_req.(input_arr))
 function get_fuel_while(mass::Int)::Int
     total_fuel = 0
     init_fuel = get_fuel_req(mass)
-    while init_fuel >= 0
+    while init_fuel > 0
         total_fuel += init_fuel
         init_fuel = get_fuel_req(init_fuel)
     end
@@ -27,3 +27,16 @@ print(get_fuel_while(1969) == 966)
 print(get_fuel_while(100756) == 50346)
 
 output2 = sum(get_fuel_while.(input_arr))
+
+function get_fuel_recursive(mass::Int)::Int
+    init_fuel = Int(floor(mass/3) - 2)
+    total_fuel = init_fuel > 0 ? init_fuel + get_fuel_recursive(init_fuel) : 0
+    return total_fuel
+end
+
+# tests
+print(get_fuel_recursive(14) == 2)  # expecting 2
+
+print(get_fuel_recursive(1969) == 966)
+
+print(get_fuel_recursive(100756) == 50346)
