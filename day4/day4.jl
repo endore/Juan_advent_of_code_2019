10702c680ff9f5ab9292b8944f3d9f8d4b7a4694
Juan Contreras <js.contreras@hotmail.com>
2020-01-19 12:21:42 -0500

    day4:part2 solved

--- day4/day4.jl
+++ day4/day4.jl
@@ -22,6 +22,32 @@ function isValidPW(number)
     return val && sameCheck
 end
 
+function isValidPW2(number)
+    # extract digit Array
+    digitArray = [parse(Int,i) for i in string(number)]
+
+    val = true
+    digitCount = [0 for i in 1:10]
+    # loop through array testing for decreasing violation
+    for i in 2:length(digitArray)
+        leftDigit = digitArray[i-1]
+        rightDigit = digitArray[i]
+        if i == 2
+            digitCount[leftDigit + 1] += 1 # 1-indexed
+        end
+
+        digitCount[rightDigit + 1] += 1
+
+        if rightDigit < leftDigit
+            val = false
+            break
+        end
+    end
+
+    return val && (2 in digitCount)
+end
 inputRange = tuple(130254,678275)
 
 output = sum(isValidPW.(inputRange[1]:inputRange[2]))
+
+output2 = sum(isValidPW2.(inputRange[1]:inputRange[2]))
