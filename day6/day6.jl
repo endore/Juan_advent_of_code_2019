586fa4956c982357eff9a526bd07515b86b7b2fd
Juan Contreras <js.contreras@hotmail.com>
2020-06-30 15:33:29 -0400

    Day 6: Part 2

--- day6/day6.jl
+++ day6/day6.jl
@@ -1,13 +1,7 @@
 using DelimitedFiles
 using DataFrames
-function partOne()
+function partOne(input_arr = readdlm("day6\\input.txt", ',', String))
     # part one
-    input_arr = readdlm("day6\\input.txt", ',', String)
-    return orbit_count_checksum(input_arr)
-end
-
-function orbit_count_checksum(input_arr)
-    # format input
     input_df = clean_input(input_arr)
 
     #: initiate total orbits with all unique objects in input_arr
@@ -22,8 +16,28 @@ function orbit_count_checksum(input_arr)
     #: update total_orbits_dict starting from parent = "COM" and using the parent_to_leafs
     update_orbits!(total_orbits_dict,input_df)
 
-    #return sum of total_orbits_dict values
     return sum(values(total_orbits_dict))
+end
+
+function partTwo(input_arr = readdlm("day6\\input.txt", ',', String))
+    # part two
+    input_df = clean_input(input_arr)
+
+    #: initiate total orbits with all unique objects in input_arr
+    total_orbits_dict = initiate_total_orbits_dict(input_df)
+
+    COM_leafs = get_leafs(input_df,"COM")
+
+    if length(COM_leafs) == 0
+        throw(DomainError(parent_to_leafs,"No COM parent entry found"))
+    end
+
+    #: update total_orbits_dict starting from parent = "COM" and using the parent_to_leafs
+    update_orbits!(total_orbits_dict,input_df)
+
+    min_transfers = min_orbit_transfers(total_orbits_dict,input_df)
+
+    return min_transfers
 end
 
 function clean_input(input)
@@ -53,4 +67,44 @@ end
 
 function get_leafs(input_df,parent)
     return input_df.Child[input_df.Parent .== parent,:]
+end
+
+function get_orbit_chain(input_df,child,list = [])
+    parent = input_df.Parent[input_df.Child .== child,:][1]
+    push!(list,parent)
+    if parent != "COM"
+        get_orbit_chain(input_df,parent,list)
+    else
+        # reverse the order once we reach COM
+        list = list[end:-1:1]
+    end
+    return list
+end
+
+function get_first_common_root(chain1,chain2)
+    chain1_rev = chain1[end:-1:1]
+    chain2_rev = chain2[end:-1:1]
+    Max = length(chain1_rev)
+    if Max < length(chain2_rev)
+        Max = length(chain2_rev)
+    end
+    fcr = "COM"
+    for i in 2:Max
+        if chain1_rev[i] == chain2_rev[i]
+            fcr = chain1_rev[i]
+        else
+            break
+        end
+    end
+    return fcr
+end
+
+function min_orbit_transfers(total_orbits,clean_input,YOU = "YOU", SAN = "SAN")
+    you_chain = get_orbit_chain(clean_input,YOU)
+    san_chain = get_orbit_chain(clean_input,SAN)
+    first_common_root = get_first_common_root(you_chain,san_chain)
+
+    min_transfers = total_orbits[YOU] + total_orbits[SAN] -2(total_orbits[first_common_root]+1)
+
+    return min_transfers
 end
--- day6/tests.jl
+++ day6/tests.jl
@@ -3,5 +3,12 @@ using Test
 
 @testset "Example 1" begin
     input = ["COM)B" "B)C" "C)D" "D)E" "E)F" "B)G" "G)H" "D)I" "E)J" "J)K" "K)L"]
-    @test orbit_count_checksum(input) == 42
+    @test partOne(input) == 42
+end
+
+@testset "Example 2" begin
+    input = ["COM)B" "B)C" "C)D" "D)E" "E)F" "B)G" "G)H" "D)I" "E)J" "J)K" "K)L" "K)YOU" "I)SAN"]
+
+    input_df = clean_input(input)
+    @test partTwo(input) == 4
 end
